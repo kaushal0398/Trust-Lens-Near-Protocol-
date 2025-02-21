a2544bd0c811fd8a86c6d888c5ccd9dba81597aa
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2025-02-21 22:41:53 +0530

    initial commit

--- near_ai_security/train_dqn.py
+++ near_ai_security/train_dqn.py
@@ -7,3 +7,10 @@ from contract_env import SmartContractEnv
 # Create AI training environment
 env = SmartContractEnv()
 
+# Load Deep Q-Network (DQN) model
+model = DQN("MlpPolicy", env, verbose=1, learning_rate=0.001, buffer_size=50000, batch_size=64)
+
+# Train AI agent on 1 million steps
+model.learn(total_timesteps=1000000)
+
+

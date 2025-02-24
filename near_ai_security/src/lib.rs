95921623e5e191b7e1f19b7e1bb5d9379009a5a9
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2025-02-24 22:47:24 +0530

    updated

--- near_ai_security/src/lib.rs
+++ near_ai_security/src/lib.rs
@@ -31,3 +31,12 @@ impl TrustScoreContract {
         self.contract_scores.get(&entity_id).or(self.user_scores.get(&entity_id))
     }
 }
+
+#[near_bindgen]
+impl TrustScoreContract {
+    pub fn dispute_audit(&mut self, contract_id: AccountId, dispute_reason: String) {
+        let mut score = self.contract_scores.get(&contract_id).unwrap_or(50);
+        score = if dispute_reason.contains("false positive") { score - 10 } else { score + 10 };
+        self.contract_scores.insert(&contract_id, &score);
+    }
+}

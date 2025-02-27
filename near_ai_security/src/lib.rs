e6aeb5b64a2b2ab1147d65f77bb780a8f8f93cf8
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2025-02-27 22:26:46 +0530

    updated Dao smart contract

--- near_ai_security/src/lib.rs
+++ near_ai_security/src/lib.rs
@@ -35,6 +35,7 @@ impl TrustScoreContract {
 #[near_bindgen]
 impl TrustScoreContract {
     pub fn dispute_audit(&mut self, contract_id: AccountId, dispute_reason: String) {
+        let mut score = self.contract_scores.get(&contract_id).unwrap_or(50);
         score = if dispute_reason.contains("false positive") { score - 10 } else { score + 10 };
         self.contract_scores.insert(&contract_id, &score);
     }

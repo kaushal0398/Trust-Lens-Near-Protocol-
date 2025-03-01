a48a363675835a77d734ba2fe54fb53f21fea934
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2025-03-01 23:36:58 +0530

    updated Dao smart contract

--- near_ai_security/contract_env.py
+++ near_ai_security/contract_env.py
@@ -10,6 +10,7 @@ class SmartContractEnv(gym.Env):
         super(SmartContractEnv, self).__init__()
         self.observation_space = spaces.Box(low=0, high=1, shape=(10,), dtype=np.float32)
         self.action_space = spaces.Discrete(3)  # 3 Actions: Safe, Medium Risk, High Risk
+        self.state = np.random.rand(10)  # Randomized contract security features
         self.contracts_scanned = 0
 
     def step(self, action):

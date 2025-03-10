65a5cf19d2b8b594dcfe69196cca661986ceae23
Kaushal <kaushalpanpalia821@gmail.com>
2025-03-10 23:43:46 +0530

    New update

--- near_ai_security/adversarial_ai.py
+++ near_ai_security/adversarial_ai.py
@@ -4,7 +4,7 @@ from eth_brownie import compile_source
 def generate_exploit():
     """Simulates an adversarial attack on smart contracts"""
     attack_templates = [
-        "function attack() public { target.call{value: 1 ether}(); }",
+        
         "function attack() public { selfdestruct(target); }",
         "function attack() public { while(true) { target.call(); } }"
     ]

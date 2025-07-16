b0eb7ab0fa2e193d210092d0c54dcd1fce8d0fd6
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2025-07-16 23:49:28 +0530

    Update lib.rs

--- near_ai_security/src/lib.rs
+++ near_ai_security/src/lib.rs
@@ -3,11 +3,7 @@ use borsh::{BorshDeserialize,
 BorshSerialize}; // 
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct TrustScoreContract {
-    contract_scores: LookupMap<AccountId, u8>,
-    user_scores: LookupMap<AccountId, u8>,
-}
+
 
 impl Default for TrustScoreContract {
     fn default() -> Self {
